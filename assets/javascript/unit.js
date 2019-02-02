a1a53e4be0eb1314c08fc39d413c66cffdb26cab
Jonathan Hernandez <jon.hernandez169@gmail.com>
2019-02-01 20:43:01 -0600

    commit 2

--- assets/javascript/unit.js
+++ assets/javascript/unit.js
@@ -2,52 +2,363 @@
 //jedi
 const luke={
     attack:10,
+    name:"luke",
+    selected:"assets\sounds\vaderselected.mp3"
+    win:
+    fail:,
+    health:200,
+    maxpower:100,
+    type:"jedi",
     mainFighter:false,
+    oppenent:false,
     get counter(){
         return this.attack / 3
     }
 }
+
 const mace={
     attack:8,
+    health:200,
+    maxpower:90,
+    name:"mace",
+    type:"jedi",
     mainFighter:false,
+    oppenent:false,
     get counter(){
         return this.attack / 2
     }
 }
 const yoda={
     attack:4,
+    health:200,
+    maxpower:75,
+    name:"yoda",
+    type:"jedi",
     mainFighter:false,
+    oppenent:false,
     get counter(){
-        return this.attack *.8
+        return this.attack *.9
     }
 }
 
 
 //sith
-const darth={
+const sidious={
     attack:10,
+    name:"sidious",
+    maxpower:100,
+    health:200,
+    type:"sith",
     mainFighter:false,
+    oppenent:false,
     get counter(){
         return this.attack / 3
     }
 }
 const vader={
     attack:8,
+    health:200,
+    maxpower:90,
+    name:"vader",
+    type:"sith",
     mainFighter:false,
+    oppenent:false,
     get counter(){
         return this.attack / 2
     }
 }
 const kylo={
     attack:4,
+    health:200,
+    maxpower:75,
+    name:"kylo",
+    type:"sith",
     mainFighter:false,
+    oppenent:false,
     get counter(){
-        return this.attack *.8
+        return this.attack *.9
+    }
+}
+//picture links in array
+const lukep="https://github.com/Jonathan169/Unit-4-game/blob/master/assets/images/luke.jpg?raw=true";
+const macep="https://github.com/Jonathan169/Unit-4-game/blob/master/assets/images/macewindu.jpg?raw=true";
+const yodap="https://github.com/Jonathan169/Unit-4-game/blob/master/assets/images/master_yoda_star_wars-t2.jpg?raw=true";
+const sidiousp="https://github.com/Jonathan169/Unit-4-game/blob/master/assets/images/darthsidious.jpg?raw=true";
+const vaderp="https://github.com/Jonathan169/Unit-4-game/blob/master/assets/images/vardercool.jpg?raw=true";
+const kylop="https://github.com/Jonathan169/Unit-4-game/blob/master/assets/images/kylo.jpg?raw=true";
+const pics=[lukep,macep,yodap,sidiousp,vaderp,kylop];
+
+
+//variables for game play
+const sith=[sidious, vader, kylo]
+const jedi=[luke,mace,yoda]
+let fallen="";
+let Coppenent="";
+let playerChosen="";
+let player="";
+let undo="";
+let battlestarted=false;
+let enumber="";
+let mynum="";
+const hbar=[$("#sithhealth"),$("#jedihealth")]
+const side=[$("#siths"),$("#jedis")]
+const flist=["luke","mace","yoda","sidious","vader","kylo"]
+const fighters=[luke,mace,yoda,sidious,vader,kylo]
+const ids=[$("#luke"),$("#mace"),$("#yoda"),$("#sidious"),$("#vader"),$("#kylo")]
+
+
+//onclick
+$("#sidious").on("click", function(){
+    playerChosen=this.id;
+    start();
+    check();
+    oppenent();
+});
+
+$("#vader").on("click", function(){
+    playerChosen=this.id;
+    start();
+    check();
+    oppenent();
+});
+
+$("#kylo").on("click", function(){
+    playerChosen=this.id;
+    start();
+    check();
+    oppenent();
+});
+
+$("#luke").on("click", function(){
+    playerChosen=this.id;
+    start();
+    check();
+    oppenent();
+});
+
+$("#mace").on("click", function(){
+    playerChosen=this.id;
+    start();
+    check();
+    oppenent();
+});
+
+$("#yoda").on("click", function(){
+    playerChosen=this.id;
+    start();
+    check();
+    oppenent();
+});
+
+//begining game functions
+function start(id){
+    for(let i=0;i<fighters.length;i++){
+    if(playerChosen===flist[i]&& player===""){
+        fighters[i].mainFighter=true;
+        player=i;
+    }}
+    if(fighters[player].type==="sith"){
+        enumber=1;
+        mynum=0;
+    }else if(fighters[player].type==="jedi"){
+        enumber=0;
+        mynum=1;
+    }
+};
+
+function check(){
+for(let i=0;i<fighters.length;i++){
+    if(fighters[i].mainFighter===true&&fighters[i].type==="jedi"){
+        $("#jedis").children('img').attr('src',pics[i]);
+        $("#jedis").children('div').attr('class',"stats");
+        $("#jname").text(flist[i]);
+        $("#jattack").text("Attack:"+fighters[i].attack);
+        ids[i].children('img').attr('src', "https://github.com/Jonathan169/Unit-4-game/blob/master/assets/images/grey.jpg?raw=true");
+        ids[i].children('div').attr("class","bstats");
+    }
+    else if(fighters[i].mainFighter===true&&fighters[i].type==="sith"){
+        $("#siths").children('img').attr('src',pics[i]); 
+        $("#siths").children('div').attr('class',"estats");
+        $("#sname").text(flist[i]);
+        $("#sattack").text("Attack:"+fighters[i].attack);
+        ids[i].children('img').attr('src', "https://github.com/Jonathan169/Unit-4-game/blob/master/assets/images/grey.jpg?raw=true");
+        ids[i].children('div').attr("class","bstats");
+    }
+}};
+
+function oppenent(){
+   playerChosen= flist.findIndex(function(word){return word===playerChosen;})
+    if(player!==""&& fighters[playerChosen].type!== fighters[player].type){
+       fighters[playerChosen].oppenent=true;
+    };
+    if (fighters[playerChosen].oppenent===true&& fighters[playerChosen].type==="jedi"&& Coppenent===""){
+        $("#jedis").children('img').attr('src',pics[playerChosen]);
+        $("#jedis").children('div').attr('class',"stats");
+        $("#jname").text(flist[playerChosen]);
+        $("#jattack").text("Attack:"+fighters[playerChosen].attack);
+        ids[playerChosen].children('img').attr('src', "https://github.com/Jonathan169/Unit-4-game/blob/master/assets/images/grey.jpg?raw=true");
+        ids[playerChosen].children('div').attr("class","bstats");
+        Coppenent=fighters[playerChosen];
+        gameReady();
+    } else if(fighters[playerChosen].oppenent===true&& fighters[playerChosen].type==="sith"&& Coppenent===""){
+        $("#siths").children('img').attr('src',pics[playerChosen]);
+        $("#siths").children('div').attr('class',"estats");
+        $("#sname").text(flist[playerChosen]);
+        $("#sattack").text("Attack:"+fighters[playerChosen].attack);
+        ids[playerChosen].children('img').attr('src', "https://github.com/Jonathan169/Unit-4-game/blob/master/assets/images/grey.jpg?raw=true");
+        ids[playerChosen].children('div').attr("class","bstats");
+        Coppenent=fighters[playerChosen];
+        gameReady();
+    };
+    updateStats(); 
+};
+
+
+//Action
+function gameReady(){
+    battlestarted=true;
+
+};
+let coChoice;
+let oattack;
+let ocounter;
+let smove;;
+
+
+function comove(){
+    oattack="";
+    ocounter="";
+    smove="";
+    coChoice="";
+    coChoice=Math.round(Math.random()*1);
+    smove=Math.round(Math.random()*2);
+    
+    if(coChoice===1){
+    oattack=true;
+    }
+    if(coChoice===0){
+        ocounter=true;
+
+    } 
+    if(coChoice===0){
+        ocounter=true;
+     }
+};
+
+
+//attack and counter functions
+function updateStats(){
+    if(fighters[player].type==="jedi"){
+        $("#jattack").text("Attack: "+fighters[player].attack);
+        $("#sattack").text("Attack: "+Coppenent.attack);
+    }
+    else if(fighters[player].type==="sith"){
+        $("#sattack").text("Attack:"+fighters[player].attack);
+        $("#jattack").text("Attack:"+Coppenent.attack);
+    };
+    hbar[enumber].width(Coppenent.health)
+    hbar[mynum].width(fighters[player].health)
+    
+};
+
+function powup(){
+    if(fighters[player].attack< fighters[player].maxpower){
+    fighters[player].attack=fighters[player].attack *2
+    }
+    else if(fighters[player].attack>=fighters[player].maxpower){
+        fighters[player].attack=fighters[player].maxpower
+        console.log("Im over 9000!!!!!")
+    }
+};
+
+function oppUp(){
+    if(Coppenent.attack<Coppenent.maxpower){
+        Coppenent.attack=Coppenent.attack *2
+    } 
+    else if(Coppenent.attack>=Coppenent.maxpower){
+        Coppenent.attack=Coppenent.maxpower
+        console.log("hes over 9000!!!!!")
+    }
+};
+
+function oppD(){
+    if( Coppenent.health<=0){
+        fighters[player].health+=50;
+        Coppenent.oppenent=false;
+        side[enumber].children("p").text(Coppenent.name+ " DEFEATED");
+        side[enumber].children('img').attr('src', "https://github.com/Jonathan169/Unit-4-game/blob/master/assets/images/grey.jpg?raw=true");
+        side[enumber].children('div').attr("class","bstats");
+        updateStats();
+        fallen++;
+        Coppenent="";
+    }
+    if(fallen===3){
+        if(confirm("the force thanks you But theres still more out there, would you like to try again")){
+            window.location.reload();
+        }
+    };
+    
+};
+function playerdef(){
+    updateStats();
+    if(fighters[player].health<=0){
+        if(confirm("AWW you lost would you like to try again")){
+            window.open("file:///C:/Users/jonat/Documents/codingBootcamp/myRepo/Unit-4-game/unit.html")
+        }
     }
 }
 
+
 //onclick
-$("#darth").on("click", function(){
-    darth.mainFighter=true;
-    $('#darth').children('img').attr('src', 'grey.jpg');
-})
+$("#attack").on("click",function(){
+    comove();
+
+    if(ocounter===true && smove===2){
+        fighters[player].health= fighters[player].health - Coppenent.counter;
+        
+        console.log("opp takes no damage")
+    }
+    else if(ocounter===true){
+        Coppenent.health=Coppenent.health - (fighters[player].attack /4);
+        fighters[player].health= fighters[player].health - Coppenent.counter;
+    }
+    else if (oattack===true){
+    fighters[player].health = fighters[player].health - Coppenent.attack;
+    Coppenent.health= Coppenent.health - fighters[player].attack ;
+    oppUp();
+    };
+    
+    powup();
+    updateStats();
+    oppD();
+    playerdef();
+});
+
+$("#counter").on("click",function(){
+
+    comove();
+    if (oattack===true&& smove===2){
+    Coppenent.health= Coppenent.health - fighters[player].counter ;
+    oppUp();
+    console.log("i take no damage");
+    }
+    else if (oattack===true){
+    fighters[player].health= fighters[player].health - (Coppenent.attack /4);
+    Coppenent.health= Coppenent.health - fighters[player].counter ;
+    console.log("i lose " +Coppenent.attack/4);
+        oppUp();
+    }
+    else if(ocounter===true){
+        Coppenent.health=Coppenent.health - 10;
+        fighters[player].health= fighters[player].health - 10;
+        fighter[player].attack= fighters[player].attack /2;
+        Coppenent.attack=Coppenent.attack /2;
+        console.log("we both counter")
+    };
+    updateStats();
+    oppD();
+    playerdef();
+});
+
+
+
